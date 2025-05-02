16b1b29c43ff2ae97b8af66e210adf3f0c22c220
Dan Bagrin <bagrin.danu@gmail.com>
2025-05-02 13:24:49 +0300

    Bagrin Dan: Finisarea implementarii productModel

--- WEB-Proje.BussinesLogic/DBModel/ProductContext.cs
+++ WEB-Proje.BussinesLogic/DBModel/ProductContext.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data.Entity;
 using WEB_Proje.Domain.Entities;
 
 namespace WEB_Proje.BussinesLogic.DBModel {
-    internal class ProductContext : DbContext{
+    public class ProductContext : DbContext{
         public ProductContext() : base("name=Electroland") {
 
         }
-        public virtual DbSet<UDdProducts> Users { get; set; }
+        public virtual DbSet<UDdProducts> Products { get; set; }
     }
 }
