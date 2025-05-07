43931275e7327f89ae24e4f13ba0903aaa2383e1
Dan Bagrin <bagrin.danu@gmail.com>
2025-05-07 20:24:46 +0300

    bagrin: a fost adaugat cosul

--- WEB-Proje.BussinesLogic/DBModel/ProductContext.cs
+++ WEB-Proje.BussinesLogic/DBModel/ProductContext.cs
@@ -3,7 +3,7 @@ using WEB_Proje.Domain.Entities;
 
 namespace WEB_Proje.BussinesLogic.DBModel {
     public class ProductContext : DbContext{
-        public ProductContext() : base("name=Electroland") {
+        public ProductContext() : base("name=ProductDB") {
 
         }
         public virtual DbSet<UDdProducts> Products { get; set; }
