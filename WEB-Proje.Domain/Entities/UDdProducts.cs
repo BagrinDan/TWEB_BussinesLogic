6738e4eef3348094e2bbd2ba20c646b44a478c46
Dan Bagrin <bagrin.danu@gmail.com>
2025-05-20 10:49:16 +0300

    Bagrin Dan: Domain

---
+++ WEB-Proje.Domain/Entities/UDdProducts.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace WEB_Proje.Domain.Entities {
+    [Table("ProductTable")]
+    public class UDdProducts {
+        
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public int Cantitate { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        public decimal? NewPrice { get; set; }
+
+        public bool IsOnSale { get; set; }
+
+        public string ImagePath { get; set; } 
+        public string ImageFileName { get; set; } 
+    }
+}
---
+++ WEB-Proje.Domain/Entities/UDdTable.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations; 
+using System.ComponentModel.DataAnnotations.Schema;
+using WEB_Proje.Domain.Enums;
+
+namespace WEB_Proje.Domain.Entities {
+    [Table("UserDate")]
+    public class UDdTable {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [Required]
+        [Display(Name = "Username")]
+        [Index(IsUnique = true)]
+        [StringLength(30, MinimumLength = 5, ErrorMessage = "Error Login")]
+        public string Username { get; set; }
+
+        [Required]
+        [Display(Name = "Password")]
+        [StringLength(50, MinimumLength = 8, ErrorMessage = "Error Password")]
+        public string Password { get; set; }
+
+        public URole userRole { get; set; }
+
+        // Additional info
+
+        [Display(Name = "Email")]
+        public string Email {  get; set; }
+
+        [Display(Name = "Phone")]
+        public string Phone { get; set; }
+
+    }
+}
